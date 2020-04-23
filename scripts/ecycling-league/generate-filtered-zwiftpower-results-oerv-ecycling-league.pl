9fdf95669763bc9120ed186d4543bad42819e886
Michael Kröll <michael.kroell@gmail.com>
2020-04-23 18:59:50 +0200

    Fetch primes for prime bonus points, use categories and avg_hr from filtered list

--- scripts/ecycling-league/generate-filtered-zwiftpower-results-oerv-ecycling-league.pl
+++ scripts/ecycling-league/generate-filtered-zwiftpower-results-oerv-ecycling-league.pl
@@ -16,6 +16,7 @@ use Number::Format;
 
 my $zp_live_url_pattern = 'https://zwiftpower.com/json.php?t=live&id=%d';
 my $zp_filtered_url_pattern = 'https://zwiftpower.com/cache3/results/%d_view.json';
+my $zp_primes_url_pattern = 'https://www.zwiftpower.com/api3.php?do=event_primes&zid=%d';
 
 my $aktive_licenses = 'AktiveLizenzen.csv';
 my $aktive_bikecards = 'AktiveBikecards.csv';
@@ -25,13 +26,24 @@ my @result_csv_field_order = (
     'eliga_category_position', 'position', 'last_name', 'first_name', 'zwift_category', 'eliga_category', 'kategorie national',
     'uciid', 'jahrgang', 'nationalität', 'club', 'race_time_formatted',
     'eliga_category_timegap', 'wkg', 'race_time', 'male', 'fin', 'dq', 'avg_hr', 'flag',
-    'filtered_by_zwiftpower', 'normalized_name',
+    'filtered_by_zwiftpower', 'normalized_name', 'primes_points'
 );
 
 binmode( STDOUT, ':encoding(UTF-8)' );
 
 my $q = CGI::Simple->new;
 my $zpid = $q->param( 'zpid' );
+
+our @relevant_primes;
+our $primes_bonus_points = $q->param( 'primes_bonus_points' );
+$primes_bonus_points ||= 25;
+my $rprimes = $q->param( 'relevant_primes' );
+if ( defined $rprimes and length $rprimes) {
+    @relevant_primes = split(',', $rprimes);
+}
+else {
+    @relevant_primes = qw(4 8 12);
+}
 
 my $csv = Text::CSV_XS->new( { auto_diag => 1, binary => 1 } );
 my $json = Cpanel::JSON::XS->new();
@@ -88,23 +100,21 @@ else {
 
     my $zwift_power_results = fetch_json($json_url);
     my $zwift_power_results_filtered = fetch_json($json_filtered_url);
-    my %filtered_names;
+    my %filtered;
     foreach my $record ( @{$zwift_power_results_filtered->{data}} ) {
-        $filtered_names{normalize_name($record->{name})} = 1;
+        $filtered{normalize_name($record->{name})} = $record;
     }
 
     my %fastest_per_eliga_category;
     my %eliga_category_positions;
 
-    #print $q->header( -status => 200, -content_type => 'text/plain; charset=utf-8', -expires => '0' );
-    print $q->header( -status => 200, -content_type => 'text/csv; charset=utf-8', -expires => '0', -content_disposition => 'inline; filename=results.csv', );
-    $csv->say(*STDOUT, \@result_csv_field_order);
+    my @output_rows;
     foreach my $record ( @{$zwift_power_results->{data}} ) {
         my $normalized_name = normalize_name($record->{name});
         my $full_row;
         if ( exists $licenses->{$normalized_name} or
              (exists $realname_mapping->{$record->{name}} and exists $licenses->{$realname_mapping->{$record->{name}}}) ) {
-            $full_row = record_to_row($record);
+            $full_row = record_to_row($record,$normalized_name,\%filtered);
             my @license_fields_to_add = ('jahrgang', 'uciid', 'kategorie national', 'nationalität', 'kategorie (uci)', 'geschlecht');
 
             if ( not exists $licenses->{$normalized_name} ) {
@@ -123,7 +133,7 @@ else {
         }
         elsif ( exists $bike_cards->{$normalized_name} or
              ( exists $realname_mapping->{$record->{name}} and exists $bike_cards->{$realname_mapping->{$record->{name}}}) ) {
-            $full_row = record_to_row($record);
+            $full_row = record_to_row($record,$normalized_name,\%filtered);
 
             if ( not exists $bike_cards->{$normalized_name} ) {
                 $normalized_name = $realname_mapping->{$record->{name}};
@@ -137,7 +147,7 @@ else {
             };
         }
         elsif ( $record->{flag} eq 'at' ) {
-            $full_row = record_to_row($record);
+            $full_row = record_to_row($record,$normalized_name,\%filtered);
             $full_row = {
                 %$full_row,
                 club => '',
@@ -149,7 +159,7 @@ else {
         if ( defined $full_row ) {
             $full_row->{normalized_name} = $normalized_name;
             $full_row->{eliga_category} = resolve_category( $full_row );
-            $full_row->{filtered_by_zwiftpower} = 1 unless exists $filtered_names{$normalized_name};
+            $full_row->{filtered_by_zwiftpower} = 1 unless exists $filtered{$normalized_name};
             my $eliga_category_position;
             if ( not $full_row->{fin} and length $full_row->{club} ) {
                 $full_row->{eliga_category_position} = 'DNF';
@@ -171,15 +181,52 @@ else {
                 $fastest_per_eliga_category{$full_row->{eliga_category}} = $full_row->{race_time};
             }
             elsif ( $full_row->{eliga_category_position} =~ /^\d+$/ ) {
+                $full_row->{eliga_finisher} = 1;
                 $full_row->{eliga_category_timegap} = format_ms( $full_row->{race_time} - $fastest_per_eliga_category{$full_row->{eliga_category}} );
             }
 
-            $csv->say(*STDOUT, [( map {$full_row->{$_}} @result_csv_field_order)]);
+            push @output_rows, $full_row;
         }
+    }
+
+    #print $q->header( -status => 200, -content_type => 'text/plain; charset=utf-8', -expires => '0' );
+    print $q->header( -status => 200, -content_type => 'text/csv; charset=utf-8', -expires => '0', -content_disposition => 'inline; filename=results.csv', );
+    $csv->say(*STDOUT, \@result_csv_field_order);
+    foreach my $row ( @output_rows ) {
+        $row->{primes_points} = calculate_primes_points( \@output_rows, $row );
+        $csv->say(*STDOUT, [( map {$row->{$_}} @result_csv_field_order)]);
     }
 }
 
 exit 0;
+
+sub calculate_primes_points {
+    my ($all_rows, $full_row) = @_;
+
+    state %all_eliga_riders;
+    foreach my $row (@$all_rows) {
+        $all_eliga_riders{$row->{normalized_name}} = $row if defined $row->{eliga_category};
+    }
+
+    state $zwift_power_results = fetch_json(sprintf($zp_primes_url_pattern, $zpid));
+    my @prime_points = qw(5 3 2 1);
+    my @riders = map { "rider_$_" } (1..10);
+    my %eliga_riders_with_points;
+    foreach my $prime ( @relevant_primes ) {
+        my $i = 0;
+        foreach my $rider (@riders) {
+            last if $i == scalar(@prime_points) - 1;
+            my $current_rider = normalize_name($zwift_power_results->{data}->[$prime-1]->{$rider}->{name});
+            if ( exists $all_eliga_riders{$current_rider} ) {
+                $eliga_riders_with_points{$current_rider} = $prime_points[$i++];
+            }
+        }
+    }
+
+    my @sorted_riders = reverse sort { $eliga_riders_with_points{$a} <=> $eliga_riders_with_points{$b} } keys %eliga_riders_with_points;
+    return $primes_bonus_points if $sorted_riders[0] eq $full_row->{normalized_name};
+    return 0;
+}
 
 sub resolve_category {
     my ($full_row) = @_;
@@ -206,37 +253,21 @@ sub resolve_category {
 }
 
 sub record_to_row {
-    my ($record) = @_;
+    my ($record,$normalized_name,$filtered) = @_;
 
     my @one_to_ones = qw(flag male fin dq grp name);
     # state $number_format = Number::Format->new(
     #     -thousands_sep   => '.',
     #     -decimal_point   => ',',);
 
-    my %zwift_category_mapping = (
-        699676 => 'A',
-        699677 => 'B',
-        699678 => 'C',
-        699679 => 'D',
-        757723 => 'A',
-        757724 => 'B',
-        757725 => 'C',
-        757726 => 'D',
-        757727 => 'E',
-        817400 => 'A',
-        817401 => 'B',
-        817402 => 'C',
-        817403 => 'D',
-    );
-
     return {
         race_time => $record->{race_time}->[0],
         race_time_formatted => format_ms($record->{race_time}->[0]),
-        avg_hr => $record->{ahr}->[0],
+        avg_hr => $filtered->{$normalized_name}->{ahr}->[0],
         #wkg => $number_format->format_number($record->{wkg}->[0]),
         wkg => $record->{wkg}->[0],
         position => $record->{fin} ? $record->{position} : 'DNF',
-        zwift_category => $zwift_category_mapping{$record->{grp}},
+        zwift_category => $filtered->{$normalized_name}->{category},
         map { $_ => $record->{$_} } @one_to_ones,
     };
 }
@@ -255,6 +286,7 @@ sub format_ms {
 sub fetch_json {
     my ($url) = @_;
 
+    warn "Fetching $url";
     my $response = $ua->get($url);
     if ( not $response->is_success ) {
         print $q->header( -status => 500 );
