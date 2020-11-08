f3e03c9c08e6cd8277f3298138b873acac1ec6bf
Michael Kröll <michael.kroell@gmail.com>
2020-11-08 09:41:09 +0100

    Missed one uppercase Team

--- scripts/ecycling-league/generate-filtered-zwiftpower-results-oerv-ecycling-league.pl
+++ scripts/ecycling-league/generate-filtered-zwiftpower-results-oerv-ecycling-league.pl
@@ -248,7 +248,7 @@ else {
             $full_row = {
                 %$full_row,
                 club => (defined $bike_cards->{$normalized_name}->{'team'} and length $bike_cards->{$normalized_name}->{'team'})
-                    ? $bike_cards->{$normalized_name}->{'Team'}
+                    ? $bike_cards->{$normalized_name}->{'team'}
                     : $bike_cards->{$normalized_name}->{'bike card'},
                 'kategorie national' => $bike_cards->{$normalized_name}->{'bike card'},
                 last_name => $bike_cards->{$normalized_name}->{'nachname'},
