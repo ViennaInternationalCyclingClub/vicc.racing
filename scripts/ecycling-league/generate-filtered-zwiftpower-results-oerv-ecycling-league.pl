2974bad2a34dc5f8b83406be5f415146a282ebb9
Michael Kröll <michael.kroell@gmail.com>
2020-11-14 13:04:08 +0100

    No mandatory HR for YOUTH category

--- scripts/ecycling-league/generate-filtered-zwiftpower-results-oerv-ecycling-league.pl
+++ scripts/ecycling-league/generate-filtered-zwiftpower-results-oerv-ecycling-league.pl
@@ -338,7 +338,7 @@ else {
                 $full_row->{eliga_category_position} = 'UNCATEGORIZED';
             }
             # Do not require HRM for Juniors
-            elsif ( not $full_row->{avg_hr} and not $full_row->{eliga_category} =~ /^JUNIORS/
+            elsif ( not $full_row->{avg_hr} and not $full_row->{eliga_category} =~ /^YOUTH/
                 # white-list riders which proved they actually have ridden with HR data
                 and not $full_row->{normalized_name} =~ /(konczer|gratzer|janecka|brunhofer|daniel hager)/ ) {
                 $full_row->{eliga_category_position} = 'DSQ';
