817359a3448ad786ab3a5ec040913d295e572681
Michael Kröll <michael.kroell@gmail.com>
2023-02-22 18:46:44 +0100

    There is a new column "team_result" right before the first Strava column

--- scripts/strava/update_teamcalendar_results_from_strava.pl
+++ scripts/strava/update_teamcalendar_results_from_strava.pl
@@ -42,7 +42,7 @@ $target_spreadsheet_name ||= 'RaceParticipations';
 my @rider_columns = qw(ID Name Nickname ZP-FTP-Category StravaID ZwiftPowerID);
 my @race_columns = ('Rider ID','Race ID','Race Start Date','Race Title','Sortable Combined Race Title','Licensed','Rider','StravaID','StartDateISO8661','EndDateISO8661');
 my @strava_activity_columns = qw(id name total_elevation_gain suffer_score average_watts weighted_average_watts max_watts moving_time elapsed_time start_date_local distance);
-my $strava_gsheet_column_index_start = 13;
+my $strava_gsheet_column_index_start = 14;
 
 my %rider_column_lookup;
 my $i = 0;
